4d747c13935eab85ea07226043ddfa86f18295f2
Reinier Ruiz Ramos <reinier@revolico.com>
2019-07-28 14:05:32 -0400

    Creating rate component

---
+++ src/components/Rate/Rate.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Star from '@material-ui/icons/Star';
+
+/**
+ * @description
+ * Component for display an score.
+ * @param {Object} - - React props.
+ * */
+const Rate = ({ score }) => {
+  const arr = [1, 2, 3, 4, 5];
+  return (
+    <React.Fragment>
+      {arr.map(item => (
+        <Star
+          color={item <= score ? 'action' : 'disabled'}
+          primary
+          key={item}
+        />
+      ))}
+    </React.Fragment>
+  );
+};
+
+/**
+ * @description
+ * Rate propTypes.
+ * @param {Number} score - Score number.
+ * */
+Rate.propTypes = {
+  score: PropTypes.number.isRequired,
+};
+
+export default Rate;
